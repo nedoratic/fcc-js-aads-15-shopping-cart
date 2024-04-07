cc2394561074957c646d959f1ca867c2f96b5f04
nedoratic <nedoratic@gmail.com>
2024-04-07 18:40:52 +0200

    index.js - product rendered on the webpage

--- index.js
+++ index.js
@@ -88,3 +88,15 @@ const products = [
 		category: 'Cupcake',
 	},
 ];
+
+// Render Products to DOM
+products.forEach(({ name, id, price, category }) => {
+	dessertCards.innerHTML += `
+		<div class="dessert-card">
+			<h2>${name}</h2>
+			<p class="dessert-price">$${price}</p>
+			<p class="product-category">Category: ${category}</p>
+			<button id="${id}" class="btn add-to-cart-btn">Add to cart</button>
+		</div>
+	`;
+});
