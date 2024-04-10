007d3994efb91a6e761743850bd338566a4ccf29
nedoratic <nedoratic@gmail.com>
2024-04-10 20:26:31 +0200

    add item method added to chopping cart class

--- index.js
+++ index.js
@@ -108,4 +108,27 @@ class ShoppingCart {
 		this.total = 0;
 		this.taxRate = 8.25;
 	}
+
+	addItem(id, products) {
+		const product = products.find((item) => item.id === id);
+		const { name, price } = product;
+		this.items.push(product);
+
+		const totalCountPerProduct = {};
+		this.items.forEach((dessert) => {
+			totalCountPerProduct[dessert.id] = (totalCountPerProduct[dessert.id] || 0) + 1;
+		});
+
+		const currentProductCount = totalCountPerProduct[product.id];
+		const currentProductCountSpan = document.getElementById(`product-count-for-id${id}`);
+
+		currentProductCount > 1
+			? (currentProductCountSpan.textContent = `${currentProductCount}x`)
+			: (productsContainer.innerHTML += `
+				<div id="dessert${id}" class="product">
+					<p><span class="product-count" id="product-count-for-id${id}"></span>${name}</p>
+					<p>${price}</p>
+				</div>
+			`);
+	}
 }
