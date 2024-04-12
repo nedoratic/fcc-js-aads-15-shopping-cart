7daae3cec02c32653001bf487cec123386bac68e
nedoratic <nedoratic@gmail.com>
2024-04-12 22:15:00 +0200

    index.js - calculate total method added to shopping cart class

--- index.js
+++ index.js
@@ -139,6 +139,16 @@ class ShoppingCart {
 	calculateTaxes(amount) {
 		return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
 	}
+
+	calculateTotal() {
+		const subTotal = this.items.reduce((total, item) => total + item.price, 0);
+		const tax = this.calculateTaxes(subTotal);
+		this.total = subTotal + tax;
+		cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+		cartTaxes.textContent = `$${tax.toFixed(2)}`;
+		cartTotal.textContent = `$${this.total.toFixed(2)}`;
+		return this.total;
+	}
 }
 
 // Cart Variable
